9310de200d7dde61f1dbfeefbe7589b20704b6df
{IamVks} <{vkshegde11@gmail.com}>
2020-10-06 18:13:20 +0530

    Updates

---
+++ precache-manifest.feb8b36754e44689853c77b257211b6d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fd6c620b6b0da47e7afd8c50fe1582a",
+    "url": "/Todolist/index.html"
+  },
+  {
+    "revision": "cb22627608675a443a4c",
+    "url": "/Todolist/static/css/main.bf9e847f.chunk.css"
+  },
+  {
+    "revision": "44df0583b198c180eaea",
+    "url": "/Todolist/static/js/2.17ff5833.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Todolist/static/js/2.17ff5833.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cb22627608675a443a4c",
+    "url": "/Todolist/static/js/main.3e39174b.chunk.js"
+  },
+  {
+    "revision": "de0994c2dd1d605bedab",
+    "url": "/Todolist/static/js/runtime-main.c2bff20a.js"
+  }
+]);
